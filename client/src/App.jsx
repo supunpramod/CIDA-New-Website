024b220b9201e4ba93743732677fe346ece1f625
supunpramod <pramodsupun06@gmail.com>
2025-08-26 15:04:04 +0530

    use the react router dom for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './Components/Header';
-import './App.css';
 import Home from './Components/Home page/Home';
 import Footer from './Components/Footer';
 
+// extra example pages
+// import About from './Components/About';
+// import Contact from './Components/Contact';
 
 function App() {
   return (
-    <div >
-      <Header />
-      <Home/>
-      <Footer/>
-     
+    <Router>
+      <div>
+        {/* Top header eka */}
+        <Header />
 
-     
-    </div>
+        {/* Page Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+
+        {/* Footer eka */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
